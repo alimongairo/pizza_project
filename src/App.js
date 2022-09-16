acecdb4e645eedbe81612518d9e5a252b946e378
alim <pasmanikirina29@gmail.com>
2022-09-16 15:58:20 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import './styles/App.css';
 
-
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="wrapper">
+      <Header />
       <h1>text</h1>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      <div className="header">
+          <div className="header__logo">
+            <h1>Project</h1>
+          </div>
+          <div className="header__cart">
+            <a href="/cart.html">
+              <img src="img/cart.png" alt="cart" width={25}/>
+            </a>
+          </div>
+      </div>
+    );
+  }
+
+  export default Header;
