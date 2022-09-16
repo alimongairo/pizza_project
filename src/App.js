8c68d6d9a30d844ba35e2d6fe6f1cefd4bde5787
alim <pasmanikirina29@gmail.com>
2022-09-16 14:07:20 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import './styles/App.css';
+
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>text</h1>
+    </div>
+  );
+}
+
+export default App;
